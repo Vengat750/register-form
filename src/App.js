11cb9b68c179b1396df4ed737f394438cbc38ab7
Vengat750 <venkivkl008@gmail.com>
2020-10-18 12:59:17 +0530

    deploy register app to github pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import PersonalDetails from './pages/personal-details';
+import EmailVerification from './pages/email-verification';
+import Successfull from './pages/successfull';
+import CompanyDetails from './pages/company-details';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route exact path="/" component={PersonalDetails} />
+          <Route path="/company-details" component={CompanyDetails} />
+          <Route path="/email-verification" component={EmailVerification} />
+          <Route path="/sucessfully-registered" component={Successfull} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
+
